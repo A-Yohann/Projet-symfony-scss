165592a01c29a355f791b1fffd63817b18b35044
Yohann <at.yohann@gmail.com>
2026-02-11 15:01:30 +0100

    [FIX] change version

--- migrations/Version20260211140041.php
+++ migrations/Version20260211140041.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260122123456 extends AbstractMigration
+final class Version20260211140041 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,9 @@ final class Version20260122123456 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE categorie_horloge ADD CONSTRAINT FK_2519D16FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE categorie_horloge ADD CONSTRAINT FK_2519D16F3FC0D758 FOREIGN KEY (mecanisme_id) REFERENCES mecanisme (id)');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CD9821596 FOREIGN KEY (categorie_horloge_id) REFERENCES categorie_horloge (id)');
         $this->addSql('ALTER TABLE conseil_conservation ADD CONSTRAINT FK_7A24AFA160BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE conseil_conservation ADD CONSTRAINT FK_7A24AFA1BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie_horloge (id)');
         $this->addSql('ALTER TABLE cotisation ADD CONSTRAINT FK_AE64D2EDA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
@@ -27,12 +30,14 @@ final class Version20260122123456 extends AbstractMigration
         $this->addSql('ALTER TABLE piece_horlogere ADD CONSTRAINT FK_EF67AEEEBCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie_horloge (id)');
         $this->addSql('ALTER TABLE piece_horlogere_mecanisme ADD CONSTRAINT FK_16DFCB69266F5DE9 FOREIGN KEY (piece_horlogere_id) REFERENCES piece_horlogere (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE piece_horlogere_mecanisme ADD CONSTRAINT FK_16DFCB693FC0D758 FOREIGN KEY (mecanisme_id) REFERENCES mecanisme (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE user CHANGE date_inscription date_inscription DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE categorie_horloge DROP FOREIGN KEY FK_2519D16FA76ED395');
+        $this->addSql('ALTER TABLE categorie_horloge DROP FOREIGN KEY FK_2519D16F3FC0D758');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CD9821596');
         $this->addSql('ALTER TABLE conseil_conservation DROP FOREIGN KEY FK_7A24AFA160BB6FE6');
         $this->addSql('ALTER TABLE conseil_conservation DROP FOREIGN KEY FK_7A24AFA1BCF5E72D');
         $this->addSql('ALTER TABLE cotisation DROP FOREIGN KEY FK_AE64D2EDA76ED395');
@@ -40,6 +45,5 @@ final class Version20260122123456 extends AbstractMigration
         $this->addSql('ALTER TABLE piece_horlogere DROP FOREIGN KEY FK_EF67AEEEBCF5E72D');
         $this->addSql('ALTER TABLE piece_horlogere_mecanisme DROP FOREIGN KEY FK_16DFCB69266F5DE9');
         $this->addSql('ALTER TABLE piece_horlogere_mecanisme DROP FOREIGN KEY FK_16DFCB693FC0D758');
-        $this->addSql('ALTER TABLE user CHANGE date_inscription date_inscription DATETIME NOT NULL');
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '2721ddbbfbeb5fe4908fe7a7c79a3e1a5aea88ed',
+        'reference' => '681b18a15cdac17e1892eee2aa5d000479d5c57a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '2721ddbbfbeb5fe4908fe7a7c79a3e1a5aea88ed',
+            'reference' => '681b18a15cdac17e1892eee2aa5d000479d5c57a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -565,6 +565,15 @@
             'install_path' => __DIR__ . '/../staabm/side-effects-detector',
             'aliases' => array(),
             'dev_requirement' => true,
+        ),
+        'symfony/apache-pack' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '3aa5818d73ad2551281fc58a75afd9ca82622e6c',
+            'type' => 'symfony-pack',
+            'install_path' => null,
+            'aliases' => array(),
+            'dev_requirement' => false,
         ),
         'symfony/asset' => array(
             'pretty_version' => 'v6.4.24',
